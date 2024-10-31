c57f2addec7db7d60851bce497be5b94585f86c4
zsedrick <zsedrickjavier03@gmail.com>
2024-10-31 12:35:55 +0800

    "Modifying Operations.vb in  BAL"

--- BAL/Operations.vb
+++ BAL/Operations.vb
@@ -1,3 +1,13 @@
-Public Class Operations
+Imports DAL
+Imports BEL
+Imports MySql.Data.MySqlClient ' Import the MySQL namespace
 
-End Class
+Namespace BAL
+    Public Class Operations
+        ' Use MySqlConnection instead of DbConnection
+        Public db As New MySqlConnection("SERVER=localhost; DATABASE=StudentManagement; UID=root; PASSWORD=allenjavier_03;")
+        Public info As New Informations()
+
+
+    End Class
+End Namespace
